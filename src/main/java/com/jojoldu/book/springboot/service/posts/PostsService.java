7deba052dcd4fd8698c4b866f4840c3a4e0ad1a2
JoYoungWook 조영욱 <siosio24@naver.com>
2021-05-01 14:50:33 +0900

    html js @query test // add, findAll

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.jojoldu.book.springboot.service.posts;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,11 +10,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
 
     private final PostsRepository postsRepository;
+
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
@@ -29,11 +34,17 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
         return new PostsResponseDto(entity);
     }
+
 
 
 }
